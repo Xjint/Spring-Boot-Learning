2473aa49c33091e795e4e7b9cf3926cf28d069d0
bai <877549050@qq.com>
2019-09-02 09:15:10 +0800

    first commit

---
+++ src/main/java/com/bjt/xint/XintApplication.java
@@ -0,0 +1,13 @@
+package com.bjt.xint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bjt/xint/XintApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bjt.xint;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XintApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
